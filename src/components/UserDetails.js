fd794f9a23c4340d5fbf31752bfc0774318fd692
Charles-Okoeguale <egualecharles@gmail.com>
2025-01-04 16:33:51 +0100

    add loading spinner

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
@@ -12,7 +11,28 @@ export const UserDetails = () => {
         .then(data => setUser(data));
     }, [id]);
   
-    if (!user) return <div className="p-4">Loading...</div>;
+    if (!user) return <div className="p-4">
+        <svg 
+          className="animate-spin h-5 w-5 mr-3 text-blue-500" 
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle 
+            className="opacity-25" 
+            cx="12" 
+            cy="12" 
+            r="10" 
+            stroke="currentColor" 
+            strokeWidth="4"
+          />
+          <path 
+            className="opacity-75" 
+            fill="currentColor" 
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          />
+        </svg>
+    </div>;
   
     return (
       <div className="p-4 max-w-2xl mx-auto">
